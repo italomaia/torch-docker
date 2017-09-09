d55b675b1d11ab877f3cee381bffb31caf2827c3
Italo Maia <italo.maia@gmail.com>
2017-09-09 01:17:31 -0300

    Build lua53 for last;
    Cuz it has the most recent syntax

--- build.lua
+++ build.lua
@@ -1,4 +1,4 @@
 local cmd = "docker build . -t italomaia/torch:%s --build-arg TORCH_LUA_VERSION=%s"
-for _, name in pairs {'lua51', 'lua52', 'lua53', 'luajit21'} do
+for _, name in pairs {'luajit21', 'lua51', 'lua52', 'lua53'} do
     os.execute(string.format(cmd, name, name:upper()))
 end
