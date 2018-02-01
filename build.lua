d5f892da08ee9fb72bbe55d2df44371f5bcfc009
Italo Maia <italo.maia@gmail.com>
2018-01-31 21:10:30 -0300

    Build command now prints built images names

--- build.lua
+++ build.lua
@@ -1,4 +1,14 @@
-local cmd = "docker build . -t italomaia/torch:%s --build-arg TORCH_LUA_VERSION=%s"
-for _, name in pairs {'luajit21', 'lua51', 'lua52', 'lua53'} do
-    os.execute(string.format(cmd, name, name:upper()))
+local label = "italomaia/torch"
+local cmd = "docker build . --rm -t %s --build-arg TORCH_LUA_VERSION=%s"
+local names = {}
+for i, version in ipairs {'luajit21', 'lua51', 'lua52', 'lua53'} do
+  table.insert(names, label .. ':' .. version)
+  os.execute(
+    string.format(cmd, names[i], version:upper()) ..
+    (i == 1 and " --no-cache" or ""))
+end
+
+print("Docker images created:")
+for _, name in pairs(names) do
+  print('-> ' .. name)
 end
